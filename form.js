6e210e9a61824b6860d90a22d26e6c5cb72fca8d
similoluwa <simiprech@gmail.com>
2023-09-30 22:15:46 +0100

    Password

--- form.js
+++ form.js
@@ -1,25 +1,32 @@
-function add(a,b){
-    console.log(a +b)
+// function add(a,b){
+//     console.log(a +b)
+// }
+// add (3,5);
+// add (4,1);
+
+
+// function greet(name){
+//     console.log("Hello" + name)
+
+// }
+// greet("Ayo")
+
+// function doublenumber(a){
+//     console.log(a,a)
+// }
+
+// doublenumber(3)
+
+function operation (){
+    let password = document.querySelector ("#pass").value;
+    let confirmpassword = document.querySelector ("#compass").value;
+    if (password === confirmpassword) {
+        alert("correct password");
+    } else { 
+        alert ("incorrect password");
+    }
 }
-add (3,5);
-add (4,1);
 
 
-function greet(name){
-    console.log("Hello" + name)
-
-}
-greet("Ayo")
-
-function doublenumber(a){
-    console.log(a,a)
-}
-
-doublenumber(3)
-
-let password = document.queryselector("");
-console.log (usersage)
-let   =prompt("(Yes or No)");
-console.log (citizen)
 
 
